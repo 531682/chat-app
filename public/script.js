8fd77e240894dbbc7b7f9567dbd7d567938e6faa
Mitchell Ekelschot <531682@student.glu.nl>
2020-05-28 20:24:04 +0200

    room support

--- public/script.js
+++ public/script.js
@@ -3,9 +3,36 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name? ')
-appendMessage('You joined')
-socket.emit('new-user', name)
+const roomContainer = document.getElementById('room-container')
+
+if (messageForm != null) {
+    const name = prompt('What is your name? ')
+    appendMessage('You joined')
+    socket.emit('new-user', roomName, name)
+
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const message = messageInput.value
+        if(message != '') {
+            appendMessage(`You: ${message}`)
+            socket.emit('send-chat-message', roomName, message)
+            messageInput.value = ''
+        }
+    })
+}
+
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div')
+    roomElement.innerText = room
+    
+    const roomLink = document.createElement('a')
+    roomLink.href = `/${room}`
+    roomLink.innerText = 'join'
+
+    roomContainer.append(roomElement)
+    roomContainer.append(roomLink)
+})
+
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
@@ -19,15 +46,6 @@ socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`)
 })
 
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    if(message != '') {
-        appendMessage(`You: ${message}`)
-        socket.emit('send-chat-message', message)
-        messageInput.value = ''
-    }
-})
 
 function appendMessage(message) {
     const messageElement = document.createElement('div')
